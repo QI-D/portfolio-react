c4aaa1236549b66f38f8f3ceca16bb2c9d27aa0e
QI-D <jonsondang@outlook.com>
2022-11-03 15:01:22 -0700

    Create Navbar
    about, projects, skills, resume, github, linkedin
    
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,35 @@
 import React from "react";
+import Resume from "../Qi_Dang_Resume.pdf"
 
 export default function Navbar() {
   return (
     <header>
+      <div>
+        <a>
+          <a href="#about">
+            QD
+          </a>
+        </a>
+        <nav>
+          <a href="#projects">
+            Projects
+          </a>
+          <a href="#skills">
+            Skills
+          </a>
+          <a href={Resume} target="_blank">
+            Resume
+          </a>
+        </nav>
+        <a
+          href="https://github.com/QI-D" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/qi-dang/" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+      </div>
     </header>
   );
 }
