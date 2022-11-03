c0a42d9390fcc79f8fd96071f48f103fd9be3646
QI-D <jonsondang@outlook.com>
2022-11-03 15:03:48 -0700

    Add about section
    intro, pic, contact, projects
    
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,46 @@
 import React from "react";
+import Typical from 'react-typical';
+import QD from '../img/qd.jpg';
 
 export default function About() {
   return (
     <section id="about">
+      <div>
+        <div>
+          <h1>
+            Hi, I'm Qi.
+            <br />
+            <p>I'm a Vancouver, BC based{' '}
+            <Typical
+              loop={Infinity}
+              wrapper="b"
+              steps={[
+                'Software',
+                1500,
+                'Full Stack',
+                1500,
+                'DevOps',
+                1500
+              ] }
+            />
+          Developer.</p>
+          </h1>
+          <p>
+            Passionate about <strong>software development</strong>, <strong>cloud computing</strong>, and <strong>automation</strong>. My entrepreneurial background has taught me strong interpersonal, and problem-solving skill, and persistence. BCIT CIT program has given me broad technology skill set including software & web development, cloud computing, database design/management, Windows & Linux system administration, and networking. My skill set was enhanced during my 16-months Co-op experiences.
+          </p>
+          <div>
+            <a href="mailto:qid14@outlook.com" target="_blank" rel="noopener noreferrer">
+              Contact Me
+            </a>
+            <a href="#projects">
+              My Projects
+            </a>
+          </div>
+        </div>
+        <div>
+        <img alt="QD" src={QD}/>
+        </div>
+      </div>
     </section>
   );
 }
