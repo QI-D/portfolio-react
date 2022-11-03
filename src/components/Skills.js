36c9b2606597127ea8831f4baec832fc4b7a8c3c
QI-D <jonsondang@outlook.com>
2022-11-03 15:46:12 -0700

    Add skills section
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { skills } from "../skill-data";
 
 export default function Skills() {
   return (
     <section id="skills">
+      <div>
+        <div>
+          <h1>
+            Skills & Technologies
+          </h1>
+          <p>
+            Self Motivated | Quick Learner
+          </p>
+        </div>
+        <div>
+          {skills.map((skill) => (
+            <div key={skill}>
+              <div>
+                  {skill.title}:
+                  {skill.items}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
---
+++ src/skill-data.js
@@ -0,0 +1,30 @@
+export const skills = [
+  {
+    title: "Languages",
+    items: "Python, JavaScript, Java"
+  },
+  {
+    title: "Web",
+    items: "NodeJS, Express, HTML/CSS/JS, React, Bootstrap, Flask, Django"
+  },
+  {
+    title: "Cloud",
+    items: "AWS, Google Cloud, Microsoft Azure"
+  },
+  {
+    title: "Database",
+    items: "MySQL, PostgreSQL, Google Firebase, MongoDB"
+  },
+  {
+    title: "Unix/ Linux",
+    items: "VMs, Command-Line, Bash, Shell Scripting, Vi"
+  },
+  {
+    title: "VCS and DevOps",
+    items: "Git, Jenkins, Terraform, Ansible, Docker, Kubernetes"
+  },
+  {
+    title: "Communication Protocols",
+    items: "HTTP, TCP/IP, REST"
+  }
+]
