5dfe67d9f7d005cea540af1ba9fc6059202b579c
QI-D <jonsondang@outlook.com>
2022-11-03 15:56:04 -0700

    Add about css
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,11 @@ import QD from '../img/qd.jpg';
 export default function About() {
   return (
     <section id="about">
-      <div>
-        <div>
-          <h1>
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Qi.
-            <br />
+            <br className="hidden lg:inline-block" />
             <p>I'm a Vancouver, BC based{' '}
             <Typical
               loop={Infinity}
@@ -25,20 +25,22 @@ export default function About() {
             />
           Developer.</p>
           </h1>
-          <p>
+          <p className="mb-8 leading-relaxed text-lg">
             Passionate about <strong>software development</strong>, <strong>cloud computing</strong>, and <strong>automation</strong>. My entrepreneurial background has taught me strong interpersonal, and problem-solving skill, and persistence. BCIT CIT program has given me broad technology skill set including software & web development, cloud computing, database design/management, Windows & Linux system administration, and networking. My skill set was enhanced during my 16-months Co-op experiences.
           </p>
-          <div>
-            <a href="mailto:qid14@outlook.com" target="_blank" rel="noopener noreferrer">
+          <div className="flex justify-center">
+            <a href="mailto:qid14@outlook.com" target="_blank" rel="noopener noreferrer"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
               Contact Me
             </a>
-            <a href="#projects">
+            <a href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
               My Projects
             </a>
           </div>
         </div>
-        <div>
-        <img alt="QD" src={QD}/>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <img className="object-cover object-center rounded" alt="QD" src={QD}/>
         </div>
       </div>
     </section>
