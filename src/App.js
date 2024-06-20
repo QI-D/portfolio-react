214fc522a81e9e24a76b56464834b553d515d60e
Qi Dang <qid14@outlook.com>
2024-06-20 10:23:14 -0700

    remove projects
    Signed-off-by: Qi Dang <qid14@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
-      <Projects />
       <Skills />
     </main>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,9 +11,6 @@ export default function Navbar() {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white text-lg">
-            Projects
-          </a>
           <a href="#skills" className="mr-5 hover:text-white text-lg">
             Skills
           </a>
