183f31d105c1fe86bdf3d785165e086881ba670e
QI-D <jonsondang@outlook.com>
2024-11-28 14:35:40 -0800

    add and update projects
    Signed-off-by: QI-D <jonsondang@outlook.com>

--- src/components/About.js
+++ src/components/About.js
@@ -37,6 +37,10 @@ export default function About() {
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
               Contact Me
             </a>
+            <a href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              My Projects
+            </a>
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
